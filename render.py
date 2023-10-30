4d340d077aedd30c8500ba91094dffad0f47abb3
DoriaA13 <doria.achouri13@gmail.com>
2023-10-30 09:28:30 +0100

    Ajout des types sur render

--- render.py
+++ render.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plot
+from graphe import Graph
 
 class render:
     
@@ -7,7 +8,8 @@ class render:
         self.fig, self.ax = plot.subplots()
         self.ax.axis('scaled')  # Permet d'obtenir un cadrage correct
         
-    def add_segment(self, point1, point2, color):
+    def add_segment(self, point1 : tuple, point2 : tuple, color : str):
+        """Méthode qui permet d'ajouter un segment"""
         x1, y1 = point1
         x2, y2 = point2
         plot.gca().add_line(plot.Line2D((x1, x2), (y1, y2), color=color))
@@ -15,8 +17,8 @@ class render:
     def display(self):
         plot.show() # Affiche la figure
     
-    def display_graph(self, graph):
-        # Affiche un graphe en ajoutant des segments pour chaque arête
+    def display_graph(self, graph : Graph):
+        """Méthode qui affiche un graphe en ajoutant des segments pour chaque arête"""
         for node, edges in graph.items():
             for neighbor, weight in edges.items():
                 self.add_segment(node, neighbor, color='b')  # Par exemple, affiche les arêtes en bleu
