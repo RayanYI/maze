fcb30dcf0104cf1cfa03f1b0b264a6e0100094ae
DoriaA13 <doria.achouri13@gmail.com>
2023-10-30 10:17:43 +0100

    Simplification de la fonction génération1

--- generation_grille.py
+++ generation_grille.py
@@ -4,7 +4,7 @@ class Generation :
     def __init__(self):
         self.graph = Graph()
     
-    def adj_grid_not_weighted(self, width : int, height : int) -> tuple :
+    def dico_grid_not_weighted(self, width : int, height : int) -> tuple :
         """Méthode qui renvoie la matrice d'adjacence du graphe non pondéré d'une grille de largeur et de hauteur spécifiées"""
         p = 1   #Non pondéré donc on met le poids à 1 pour toutes les arêtes car on doit malgré tout utiliser un poids
         for i in range(height):
@@ -13,15 +13,10 @@ class Generation :
                 y = i
                 case_courante= (x, y)
                 self.graph.add_sommet(case_courante)   #Ajout d'une case
-            for i in range(height):
-                for j in range(width):  #Doubles boucles qui parcourent les cases de la grille
-                    x = j
-                    y = i
-                    case_courante= (x, y)
-                    self.graph.add_arete((case_courante, (x+1, y)), p)   #On essaie d'ajouter une arête à chaque case adjacente du graphe
-                    self.graph.add_arete((case_courante, (x-1, y)), p)
-                    self.graph.add_arete((case_courante, (x, y+1)), p )
-                    self.graph.add_arete((case_courante, (x, y-1)) , p)
+                self.graph.add_arete((case_courante, (x+1, y)), p)   #On essaie d'ajouter une arête à chaque case adjacente du graphe
+                self.graph.add_arete((case_courante, (x-1, y)), p)
+                self.graph.add_arete((case_courante, (x, y+1)), p )
+                self.graph.add_arete((case_courante, (x, y-1)) , p)
         return self.graph.get_adj(), self.graph.weight
     
 
