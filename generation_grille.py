065d76f8bfc5e799443dfb7b4c33fe005ff65bd0
DoriaA13 <doria.achouri13@gmail.com>
2023-10-30 14:34:56 +0100

    Génération pondérée

--- generation_grille.py
+++ generation_grille.py
@@ -1,11 +1,13 @@
 from graphe import Graph
+from random import Random
+
 
 class Generation : 
     def __init__(self):
         self.graph = Graph()
     
     def dico_grid_not_weighted(self, width : int, height : int) -> tuple :
-        """Méthode qui renvoie la matrice d'adjacence du graphe non pondéré d'une grille de largeur et de hauteur spécifiées"""
+        """Méthode qui renvoie les dictionnaires de poids et d'ajdacence du graphe non pondéré d'une grille de largeur et de hauteur spécifiées"""
         p = 1   #Non pondéré donc on met le poids à 1 pour toutes les arêtes car on doit malgré tout utiliser un poids
         for i in range(height):
             for j in range(width):  #Doubles boucles qui parcourent les cases de la grille
@@ -19,8 +21,22 @@ class Generation :
                 self.graph.add_arete((case_courante, (x, y-1)) , p)
         return self.graph.get_adj(), self.graph.weight
     
-
+    def dico_grid_weighted(self, width : int, height : int) -> tuple :
+        """Méthode qui renvoie les dictionnaires de poids et d'ajdacence du graphe pondéré aléatoirement d'une grille de largeur et de hauteur spécifiées"""  
+        for i in range(height):
+            for j in range(width):
+                random= Random()
+                p = random.uniform(0,1) 
+                x = j
+                y = i
+                case_courante= (x, y)
+                self.graph.add_sommet(case_courante)   
+                self.graph.add_arete((case_courante, (x+1, y)), p)  
+                self.graph.add_arete((case_courante, (x-1, y)), p)
+                self.graph.add_arete((case_courante, (x, y+1)), p )
+                self.graph.add_arete((case_courante, (x, y-1)) , p)
+        return self.graph.get_adj(), self.graph.weight
 g= Generation()
-print(g.adj_grid_not_weighted(3, 4)[0])
+print(g.dico_grid_weighted(3, 4)[1])
 
         
