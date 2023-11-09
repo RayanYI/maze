fc595f08df1dfb54ea4133600a9440b52f8f7d5b
RayanYI <rayanyyousfi@gmail.com>
2023-11-09 12:12:20 +0100

    ajout display_PrimTree

--- render.py
+++ render.py
@@ -3,7 +3,7 @@ from graphe import Graph
 plot.gca().get_xaxis()
 plot.gca().get_yaxis()
 axes = plot.gca()
-axes = plot.gca()
+#axes = plot.gca()
 
 
 class Render:
@@ -18,7 +18,7 @@ class Render:
         x1, y1 = point1
         x2, y2 = point2
         plot.gca().add_line(plot.Line2D((x1, x2), (y1, y2), color=color))
-        
+
     def display(self):
         plot.show() # Affiche la figure
     
@@ -31,7 +31,20 @@ class Render:
             for neighboor in dict_adj[node]:
                 self.add_segment(node, neighboor, color='b')  # Par exemple, affiche les arêtes en bleu
         self.display()
-        
+
+    def display_PrimTree(self,graph:Graph):
+        dict_adj = graph.get_adj()
+        x,y = int(axes.get_xlim()[1]),int(axes.get_ylim()[1])
+
+        for i in range(x+1):
+            for j in range(y+1):
+                if (i,j) not in dict_adj or (i,j+1) not in dict_adj[(i,j)]:
+                    self.add_segment(point1=(i-0.5,(2*j+1)/2), point2 = (i+0.5,(2*j+1)/2),color='r')
+
+                if (i,j) not in dict_adj or (i+1,j) not in dict_adj[(i,j)]:
+                    self.add_segment(point1=((2*i+1)/2,j-0.5), point2 = ((2*i+1)/2,j+0.5),color='b')
+
+        self.display()
         
 if __name__ == "__main__":
     render = Render()
