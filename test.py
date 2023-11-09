05b885b78e71c985782601f24750fbe339a44d35
DoriaA13 <doria.achouri13@gmail.com>
2023-11-09 10:28:54 +0100

    Première version de Prim

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from generation_grille import Generation
 from render import Render
+from algo_Prim import Algo
 
 
 
@@ -10,9 +11,13 @@ aff = Render()
 aff.set_axes(4, 6)
 test1.dico_grid_not_weighted(4, 6)
 aff.display_graph(test1.graph)
+ex1 = Algo(test1.graph)
+couts = test1.graph.adj.values()
+print(ex1.minimal(couts))
 
-test2 = Generation()
+
+"""test2 = Generation()
 aff2 = Render()
 aff2.set_axes(8, 4)
 test2.dico_grid_weighted(8, 4)
-aff2.display_graph(test2.graph)
+aff2.display_graph(test2.graph)"""
