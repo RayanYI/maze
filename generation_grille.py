95c683f0bb15c3fdd960fa73573da9495b295b84
RayanYI <rayanyyousfi@gmail.com>
2023-11-11 18:31:27 +0100

    ajout dico_grid_weighted_biaise tâche 6

--- generation_grille.py
+++ generation_grille.py
@@ -41,6 +41,26 @@ class Generation :
                 for move in moves:
                     self.graph.add_arete((case_courante, (x+move[0], y+move[1])), p)
 
+    def dico_grid_weighted_biaise(self, width : int, height : int, biaise_x:int = 0, biaise_y:int = 0) -> tuple :
+        """Méthode qui renvoie les dictionnaires de poids biaisé et
+         d'ajdacence du graphe pondéré aléatoirement
+         d'une grille  de largeur et de hauteur spécifiées """
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        self.width = width
+        self.height = height
+        for i in range(height + 1):
+            for j in range(width + 1):
+                random = Random()
+                p = random.uniform(0, 1)
+                x = j
+                y = i
+                case_courante = (x, y)
+                self.graph.add_sommet(case_courante)
+                for move in moves:
+                    # si on se deplace horizontalement on ajoute baise_x a p sinon biaise_y
+                    self.graph.add_arete((case_courante, (x + move[0], y + move[1])), p+abs(move[0])*biaise_x+abs(move[1])*biaise_y)
+
+
         return self.graph.get_adj(), self.graph.weight
 
 
