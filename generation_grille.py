149a1be06567321e1cab8a609fc2daad359aa6ea
RayanYI <rayanyyousfi@gmail.com>
2023-11-20 22:39:38 +0100

    add dico_hexagonal_grid_weighted

--- generation_grille.py
+++ generation_grille.py
@@ -40,6 +40,8 @@ class Generation :
                 self.graph.add_sommet(case_courante)
                 for move in moves:
                     self.graph.add_arete((case_courante, (x+move[0], y+move[1])), p)
+        return self.graph.get_adj(), self.graph.weight
+
 
     def dico_grid_weighted_biaise(self, width : int, height : int, biaise_x:int = 0, biaise_y:int = 0) -> tuple :
         """Méthode qui renvoie les dictionnaires de poids biaisé et
@@ -60,8 +62,23 @@ class Generation :
                     # si on se deplace horizontalement on ajoute biaise_x a p sinon biaise_y
                     self.graph.add_arete((case_courante, (x + move[0], y + move[1])), p+abs(move[0])*biaise_x+abs(move[1])*biaise_y)
 
-
         return self.graph.get_adj(), self.graph.weight
 
+    def dico_hexagonal_grid_weighted(self, width: int, height: int) -> tuple:
+        """Méthode qui renvoie les dictionnaires de poids et d'ajdacence du graphe pondéré aléatoirement d'une grille de largeur et de hauteur spécifiées"""
+        moves = [(1, 0), (-1, 0), (1, 1), (-1, 1),(-1,-1),(1,-1)]
+        self.width = width
+        self.height = height
+        for i in range(height + 1):
+            for j in range(width + 1):
+                random = Random()
+                p = random.uniform(0, 1)
+                x = j
+                y = i
+                case_courante = (x, y)
+                self.graph.add_sommet(case_courante)
+                for move in moves:
+                    if x+move[0] >= 0 and x+move[0] <=width and y+move[1] >=0 and y+move[1] <=height:
+                        self.graph.add_arete((case_courante, (x + move[0], y + move[1])), p)
 
-        
+        return self.graph.get_adj(), self.graph.weight
