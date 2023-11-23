20d4c86e6c84eba91a0b0648ace24882c4335061
RayanYI <rayanyyousfi@gmail.com>
2023-11-23 10:24:51 +0100

    fix biais issue

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from resolution import Resolution
 from graphe import Graph
 
 #Labyrinthe Normal
-""""
+
 grille = Generation()
 height = int(input("Quelle est la hauteur du labyrinthe souhaitée ?"))
 width = int(input("Quelle est la largeur du labyrinthe souhaitée ?"))
 biasis = input("Souhaitez-vous induire un biais (Si oui indiquer : O)")
 if biasis == "O":
-    horizontal_biasis = int(input("Indiquez les valeurs du biais horizontal"))
-    vertical_biasis = int(input("Indiquer les valeurs du biais vertical"))
+    horizontal_biasis = float(input("Indiquez les valeurs du biais horizontal"))
+    vertical_biasis = float(input("Indiquer les valeurs du biais vertical"))
     grille.dico_grid_weighted_biaise(width, height,horizontal_biasis, vertical_biasis)
 else:
     grille.dico_grid_weighted(width, height)
@@ -24,10 +24,10 @@ aff.set_axes(width, height)
 aff.display_PrimTree(laby)
 res = Resolution(laby,width,height,aff)
 res.shortestPathSolving()
-aff.display()"""
+aff.display()
 
 #Labyrinthe Hexagonale
-
+""""
 grille = Generation()
 grille.dico_hexagonal_grid_weighted(20,20)
 prim = Algo(grille.graph)
@@ -38,4 +38,4 @@ g = Graph()
 aff.draw_hexagonal_Maze(laby)
 res = Resolution(laby,20,20,aff)
 res.shortestPathSolving()
-aff.display()
+aff.display()"""
