5e260d5823e93bc01962f871d79d64d7bfc91da9
DoriaA13 <doria.achouri13@gmail.com>
2023-11-25 18:42:49 +0100

    Petit test de graphe

--- test.py
+++ test.py
@@ -3,6 +3,20 @@ from graphe import Graph
 from render import Render
 from math import inf
 from algo_Prim import Algo
+from resolution import Resolution
+
+
+#Test de graphe
+graphe1=Graph()
+graphe1.add_sommet("A")
+graphe1.add_sommet("B")
+graphe1.add_sommet("C")
+graphe1.add_sommet("D")
+graphe1.add_arete(("A","B"), 2)
+graphe1.add_arete(("A","D"), 1)
+graphe1.add_arete(("D","B"), 2)
+graphe1.add_arete(("C","D"), 2)
+print(graphe1.get_adj(), graphe1.weight)
 
 
 
@@ -24,7 +38,7 @@ test2.dico_grid_weighted(10,10)
 test = Generation()
 test.dico_grid_weighted(2, 2)
 
-###Test de Prim##
+
 test= Graph()
 algo=Algo(test2.graph)
 print(algo.prim())
@@ -32,3 +46,7 @@ tree=algo.generate_tree(test2.graph, test2.graph.weight)
 print(tree.adj)
 print(algo.parent)
 aff2.display_PrimTree(tree)
+res=Resolution(tree, 10, 10, aff2)
+res.shortestPathSolving()
+aff2.display()
+
